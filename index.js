822fa80145ca4bf701aa586567cb080116093537
Chris Masters <ccm22_@outlook.com>
2022-10-05 17:57:24 +1100

    commit tree d25950fc3980ec907534eb1a5d16ff78cc232c54

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// need notes
+
+function writeCards(names, event) {
+    const msgs = [];
+    for (let i=0; i<names.length; i++) {
+        msgs.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);    
+    }
+    return msgs;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+// While loop
+
+function countDown(number) {
+    while (number>=0) {
+        console.log(number--);
+    }
+}
